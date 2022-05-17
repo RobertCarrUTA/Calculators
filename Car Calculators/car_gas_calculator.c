95fe14ba2008df23a977378de58fc99246b7612b
Robert Carr <70494103+RobertCarrUTA@users.noreply.github.com>
2022-05-17 03:24:36 -0500

    Added two new calculator folders

---
+++ Car Calculators/car_gas_calculator.c
@@ -0,0 +1,58 @@
+/*
+    This program was made by Robert Carr. Although simple and basic, it simplifies something that I always wonder when
+	    wanting to know how much money in gas has been spent on a certain car. This program was made just for fun and it's
+	    accuracy depends on the accuracy of the information entered. The reason I made it was because while I was riding home
+	    from Big Bend National Park with a friend, I wondered how much money has been put into my friends truck on gas. This
+	    is what I came up with.
+
+    Program instructions/info:
+	    Compiled with GCC on Oracle VM Virtual Box (Linux Ubuntu (64-bit))
+	        
+	        How to compile in terminal:
+		        gcc -std=c99 -o car_gas_calculator car_gas_calculator.c
+		    
+		    How to run in the terminal:
+		        ./car_gas_calculator
+*/
+
+
+#include <stdio.h>
+
+int main()
+{
+    /*
+    To calculate the total amount of money in gas has been spent on a certain car, we have a few variables. We have to know the amount of gallons the tank can hold.
+        We can name this variable gallonsInTank. Another variable we need to use is the average miles per gallon, we can name this variable milesPerGallon. Finally,
+        we need to know the average price of gas, we can call this variable averageGasPrice.
+        
+        From the above varibles we can generate two more varibles, totalMilesPerTank and totalGallonsUsed. With all of this information we can estimate the amount
+        of money in gas has been spent on a certain car.
+    */
+    
+    printf("Hello. This is a total cost calculator for how much money has gone into your gas tank!\nPlease enter the following information.");
+    printf("\n\n\tGallons your car tank holds: ");
+    double gallonsInTank;
+    scanf("%lf", &gallonsInTank);
+    
+    printf("\tMiles per gallon for your vehicle: ");
+    int milesPerGallon;
+    scanf("%d", &milesPerGallon);
+    
+    printf("\tMiles driven: ");
+    int milesDriven;
+    scanf("%d", &milesDriven);
+    
+    printf("\tAverage gas price per gallon: ");
+    double averageGasPrice;
+    scanf("%lf", &averageGasPrice);
+    
+    // Now we do the math for generating the total cost of the gas
+    double totalMilesPerTank = gallonsInTank * milesPerGallon;
+    double totalGallonsUsed = milesDriven / totalMilesPerTank; // We need to convert total miles driven to total gallons used
+    
+    double totalCost = totalGallonsUsed * averageGasPrice;
+    
+    printf("\nWith the information you have entered for this calculator, you have put $%2.2f into your car in gas alone!\n", totalCost);
+    
+    return 0;
+}
---
+++ Math Calculators/math_calculator.c
@@ -0,0 +1,240 @@
+/*
+    This is a basic calculator that I decided to make because I was bored.
+    
+    This program was made by Robert Carr. This is a basic calculator that I decided to make because I was bored.
+
+    Program instructions/info:
+	    Compiled with GCC on Oracle VM Virtual Box (Linux Ubuntu (64-bit))
+	        
+	        How to compile in terminal:
+		        gcc -std=c99 -o math math_calculator.c
+		    
+		    How to run in the terminal:
+		        ./math
+*/
+#include <stdio.h>
+#include <math.h> // for sqrt()
+#include <locale.h> // allows for xxx,xxx,xxx... to be used in our prints of big numbers
+
+void quadratic_equation_solver();
+void pythagorean_theorem_equation_solver();
+void factorial_solver();
+
+int main()
+{
+    int solve_another_equation = 1;
+    
+    // While the user wants to continue solving equations, continue this loop
+    while(solve_another_equation == 1)
+    {
+        printf("\nPlease select the following options for an equation solver:");
+    
+        printf("\n\n\t1: ax^2 + bx + c = 0");
+        printf("\n\n\t2: a^2 + b^2 = c^2");
+        printf("\n\n\t3: n!");
+    
+        printf("\n\nPlease enter the number for the equation you want to solve: ");
+        int selection;
+        scanf("%d", &selection);
+    
+        if(selection == 1)
+        {
+            quadratic_equation_solver();
+        }
+        else if(selection == 2)
+        {
+            pythagorean_theorem_equation_solver();
+        }
+        else if(selection == 3)
+        {
+            factorial_solver();
+        }
+        else
+        {
+            printf("Please enter a valid number for an equation to solve.");  
+        }
+        
+        printf("\n\nWould you like to solve another equation?\n\t1 for Yes, 0 for No: ");
+        scanf("%d", &solve_another_equation);
+    }
+    return 0;
+}
+
+void quadratic_equation_solver()
+{
+    int solve_another_quadratic = 1;
+    while(solve_another_quadratic == 1)
+    {
+        printf("\nPlease enter the values for this equation.");
+    
+        printf("\n\tValue for a: ");
+        double a;
+        scanf("%lf", &a);
+    
+        printf("\tValue for b: ");
+        double b;
+        scanf("%lf", &b);
+    
+        printf("\tValue for c: ");
+        double c;
+        scanf("%lf", &c);
+
+        int y = 0;
+    
+        double discriminant = (b * b) - (4 * a * c);
+        double real_Number, imaginary_Number, first_Root, second_Root;
+
+        //When discriminant > 0 there are two real roots.
+        if(discriminant > 0)
+        {
+            first_Root = (-b + sqrt(discriminant)) / (2 * a);
+            second_Root = (-b - sqrt(discriminant)) / (2 * a);
+            printf("\nThe discriminant is greater than 0, so root 1 and root 2 only contain real numbers (real roots).\nTheir values are:");
+            printf("\n\troot 1 = %.2lf\n\troot 2 = %.2lf\n", first_Root, second_Root);
+        }
+        //When discriminant = 0 there is one real root.
+        else if(discriminant == 0)
+        {
+            first_Root = -b / (2 * a);
+            printf("\nThe discriminant is equal to 0, so root 1 and root 2 are equal and only contain real numbers (real roots).\nTheir values are:");
+            printf("\n\troot 1 = %.2lf\n\troot 2 = %.2lf", first_Root, first_Root);
+        }
+        //When discriminant < 0 there are two complex roots.
+        else
+        {
+            real_Number = -b / (2 * a);
+            imaginary_Number = sqrt(-discriminant) / (2 * a);
+            printf("\nThe discriminant is less than 0, so root 1 and root 2 contain imaginary numbers (complex roots).\nTheir values are:");
+            printf("\n\troot 1 = %.2lf + %.2lfi\n\troot 2 = %.2lf - %.2lfi", real_Number, imaginary_Number, real_Number, imaginary_Number);
+        }
+    
+        printf("\n\nWould you like to solve another quadratic equation?\n\t1 for Yes, 0 for No: ");
+        scanf("%d", &solve_another_quadratic);
+    }
+}
+
+void pythagorean_theorem_equation_solver()
+{
+    int solve_another_pythagorean_theorem = 1;
+    
+    while(solve_another_pythagorean_theorem == 1)
+    {
+        printf("\nWhat side of the triangle do you want to solve for?");
+        printf("\n\tA\n\tB\n\tC");
+    
+        printf("\nEnter the side you wish to solve for: ");
+        char triangle_Side;
+        scanf(" %c", &triangle_Side); // The space before %c allows for this to work
+    
+        double A, B, C;
+    
+        if(triangle_Side == 'A')
+        {
+            printf("\tEnter the sides of the triangle as they are shown:");
+            printf("\n\tB: ");
+            scanf("%lf", &B);
+        
+            printf("\n\tC: ");
+            scanf("%lf", &C);
+        
+            A = sqrt((C * C) - (B * B));
+            
+            if(C < 1 || B < 1)
+            {
+               printf("A cannot be solved because B and C have to be a positive value.\n");
+            }
+            else if(C < B)
+            {
+                printf("A cannot be solved because C has to be greater than B.\n");
+            }
+            else
+            {
+                printf("\nThe value for A is: %.2lf", A);
+            }
+        }
+        else if(triangle_Side == 'B')
+        {
+            printf("\tEnter the sides of the triangle as they are shown:");
+            printf("\n\tA: ");
+            scanf("%lf", &A);
+        
+            printf("\n\tC: ");
+            scanf("%lf", &C);
+        
+            B = sqrt((C * C) - (A * A));
+            
+            if(C < 1 || A < 1)
+            {
+               printf("B cannot be solved because A and C have to be a positive value.\n");
+            }
+            else if(C < B)
+            {
+                printf("B cannot be solved because C must be bigger than the value of A.\n");
+            }
+            else
+            {
+                printf("\nThe value for A is: %.2lf", B);
+            }
+        }
+        else if(triangle_Side == 'C')
+        {
+            printf("\tEnter the sides of the triangle as they are shown:");
+            printf("\n\tA: ");
+            scanf("%lf", &A);
+        
+            printf("\n\tB: ");
+            scanf("%lf", &B);
+        
+            C = (A * A) + (B * B);
+            
+            if(A < 1 || B < 1)
+            {
+                printf("C cannot be solved because A and B need to be positive vales.\n");
+            }
+            printf("\nThe value for C, the hypotenuse is: %.2lf", C);
+        }
+        else
+        {
+            printf("This was not a valid choice.\n");   
+        }
+        
+        printf("\n\nDo you want to solve another pythagorean theorem?\n\t1 for Yes, 0 for No:");
+        scanf("%d", &solve_another_pythagorean_theorem);
+    }
+}
+
+void factorial_solver()
+{
+    int solve_another_factorial = 1;
+    
+    while(solve_another_factorial == 1)
+    {
+        printf("\nI want to know the factorial for: ");
+        int factorial;
+        scanf("%d", &factorial);
+        
+        unsigned long long factorial_answer = 1;
+        for(int i = 1; i <= factorial; i++)
+        {
+            factorial_answer *= i;
+        }
+        
+        if(factorial < 0)
+        {
+            printf("\nThe factorial must be a positive integer.");
+        }
+        else if(factorial >= 21)
+        {
+            printf("\nOh no! The answer to the factorial is larger than 18,446,744,073,709,551,615!\n");
+            printf("\tThis means it is too big to store in the unsigned long long data type!\n");
+        }
+        else
+        {
+            setlocale(LC_ALL,""); // formating could cause issues, allows us to format output with ,'s' (xxx,xxx,xxx,...)
+            printf("\n\tThe answer to %d! is : %'llu", factorial, factorial_answer);
+        }
+        
+        printf("\nDo you want to solve another factorial?\n\t1 for Yes, 0 for No: ");
+        scanf("%d", &solve_another_factorial);
+    }
+}
